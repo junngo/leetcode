45449083cf298fec8897e1627cde6ef565d41e06
Myeongjun Go <myeongjun.ko@gmail.com>
2018-05-08 22:39:57 +0900

    rewrited algorithm

--- README.md
+++ README.md
@@ -13,9 +13,10 @@ Today I Learned
 
 
 ## algorithms
-|  #  | Title           |  Solution       | Difficulty    | Tag          | Note|
-|-----|---------------- | --------------- | ------------- |--------------|-----|
-001 | [TwoSum](https://leetcode.com/problems/two-sum//) | [Python](./algorithms/TwoSum) | Easy         |||
+|  #  | Title                  |  Solution       | Difficulty    | Tag          | Note|
+|-----|----------------------- | --------------- | ------------- |--------------|-----|
+001 | [TwoSum](https://leetcode.com/problems/two-sum/) | [Python](./algorithms/TwoSum) | Easy         |||
+002 | [Add Two Numbers](https://leetcode.com/problems/add-two-numbers/) | [Python](./algorithms/AddTwoNumbers) | Medium         |||
 
 
 ## system
--- algorithms/AddTwoNumbers/solution.py
+++ algorithms/AddTwoNumbers/solution.py
@@ -14,7 +14,7 @@ class ListNode(object):
 		self.next = None
 
 class Solution(object):
-	def addTwoNumbers(self, l1, l2):
+	def addTwoNumbers(self, l1, l2):	# bad thing
 		retLN = ListNode(0)
 		head = retLN
 		carry = 0
@@ -22,7 +22,7 @@ class Solution(object):
 		while(l1 or l2):
 			tempVal = l1.val + l2.val + retLN.val
 			tempVal, carry = tempVal%10, tempVal/10
-			
+
 			if carry==1:
 				retLN.next = ListNode(1)
 			else:
@@ -36,10 +36,38 @@ class Solution(object):
 
 		return head
 
+	def addTwoNumbers2(self, l1, l2):	# good thing
+
+		retLn = ListNode(0)
+		head = retLn
+		tempVal = 0
+		carry = 0
+
+		while(l1 or l2):
+
+			if(l1):
+				tempVal += l1.val
+				l1 = l1.next
+			if(l2):
+				tempVal += l2.val
+				l2 = l2.next
+
+			carry, tempVal = divmod(tempVal, 10)
+
+			retLn.next = ListNode(tempVal)
+			retLn = retLn.next
+			tempVal = carry
+
+		if carry == 1:
+			retLn.next = ListNode(1)
+
+		return head.next
+
+
 if __name__ == '__main__':
-	lN1, lN1.next, lN1.next.next = ListNode(2), ListNode(4), ListNode(3)
-	lN2, lN2.next, lN2.next.next = ListNode(5), ListNode(6), ListNode(4)
-	resultVar = Solution().addTwoNumbers(lN1, lN2)
+	lN1, lN1.next, lN1.next.next = ListNode(2), ListNode(4), ListNode(5)
+	lN2, lN2.next, lN2.next.next = ListNode(5), ListNode(4), ListNode(6)
+	#resultVar = Solution().addTwoNumbers(lN1, lN2)
+	resultVar = Solution().addTwoNumbers2(lN1, lN2)
 
 	print "{0} -> {1} -> {2}".format(resultVar.val, resultVar.next.val, resultVar.next.next.val)
-
