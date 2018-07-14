a404b0a5059917ede0eb733ffb5a96055d6010fc
Myeongjun Go <myeongjun.ko@gmail.com>
2018-07-14 23:14:38 +0900

    [algorithm] solving ZigZag Conversion

--- README.md
+++ README.md
@@ -19,8 +19,9 @@ Today I Learned
 002 | [Add Two Numbers](https://leetcode.com/problems/add-two-numbers/) | [Python](./algorithms/AddTwoNumbers) | Medium         |||
 003 | [Longest Substring Without Repeating Characters](https://leetcode.com/problems/longest-substring-without-repeating-characters/) | [Python](./algorithms/LongestStringWithoutRepeating) | Medium         |||
 004 | [Median of Two Sorted Arrays](https://leetcode.com/problems/median-of-two-sorted-arrays/) | [Python](./algorithms/MedianOfTwoSortedArr) | Hard         |||
+005 | [Longest Palindromic Substring](https://leetcode.com/problems/longest-palindromic-substring/) | [Python](./algorithms/LongestPalindromicSubstring) | Medium         |||
+006 | [ZigZag Conversion](https://leetcode.com/problems/zigzag-conversion/) | [Python](./algorithms/ZigZagConversion) | Medium         |||
 
 
 ## system
-
 * file Input Output
---
+++ algorithms/ZigZagConversion/solution.py
@@ -0,0 +1,61 @@
+'''
+The string "PAYPALISHIRING" is written in a zigzag pattern on a given number of rows like this: (you may want to display this pattern in a fixed font for better legibility)
+
+P   A   H   N
+A P L S I I G
+Y   I   R
+
+And then read line by line: "PAHNAPLSIIGYIR"
+
+Write the code that will take a string and make this conversion given a number of rows:
+
+string convert(string s, int numRows);
+
+Example 1:
+
+Input: s = "PAYPALISHIRING", numRows = 3
+Output: "PAHNAPLSIIGYIR"
+
+Example 2:
+
+Input: s = "PAYPALISHIRING", numRows = 4
+Output: "PINALSIGYAHRPI"
+Explanation:
+
+P     I    N
+A   L S  I G
+Y A   H R
+P     I
+'''
+
+class Solusion(object):
+    def convert(self, s, numRows):
+        arrayIdx = min(numRows, len(s))
+        tmpString = [[] for i in range(arrayIdx)]
+        retString = ""
+        curRow = 0
+        upGoing = False
+
+        if len(s) < numRows or numRows == 1:
+            return s
+
+        for c in s:
+            tmpString[curRow].append(c)
+
+            if (curRow == 0) or (curRow == (numRows - 1)):
+                upGoing = not upGoing
+
+            if upGoing:
+                curRow += 1
+            else:
+                curRow -= 1
+
+        for i in range(0, arrayIdx):
+            retString += "".join(tmpString[i])
+
+        return retString
+
+
+if __name__=='__main__':
+	str = Solusion().convert("AB", 1)
+	print(str)
