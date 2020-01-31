c4858c14d93a5d5c022d70097f5fb107d461d4e7
junngo <myeongjun.ko@gmail.com>
2020-01-31 22:45:26 +0900

    start algorithms practice leetcode #1

--- algorithms/TwoSum/python/solution.py
+++ algorithms/TwoSum/python/solution.py
@@ -6,28 +6,18 @@ Given nums = [2, 7, 11, 15], target = 9,
 Because nums[0] + nums[1] = 2 + 7 = 9,
 return [0, 1].
 """
+from typing import List
 
-class Solution(object):
-	def twoSum(self, nums, target):
-		for i in range(0, len(nums)-1):
-			for j in range(i+1, len(nums)):
-				if(nums[i]+ nums[j] == target):
-					return [i, j]
-		return "no matching num"
-
-
-	def twoSum2(self, nums, target):
-		j = 0
+class Solution:
+	def twoSum(self, nums: List[int], target: int) -> List[int]:
+		idx = 0
 
 		for i in nums:
 			find_data = target - i
-			j += 1
-			temp_nums = nums[j:]
+			idx += 1
 
-			if find_data in temp_nums:
-				return [j-1, temp_nums.index(find_data)+j]
-
+			if find_data in nums[idx:]:
+				return [idx-1, nums[idx:].index(find_data)+idx]
 
 if __name__ == '__main__':
-	#print(Solution().twoSum([2, 7, 11, 15], 9))
-	print(Solution().twoSum2([2, 7, 11, 15], 9))
+	print(Solution().twoSum([2, 7, 11, 15], 9))
