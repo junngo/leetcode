72d626f11ad18ea3fc77663d4a4bb70d344dcbaa
junngo <myeongjun.ko@gmail.com>
2020-03-08 21:53:15 +0900

    review algorithms leetcode(#2)

--- algorithms/AddTwoNumbers/solution.py
+++ algorithms/AddTwoNumbers/solution.py
@@ -13,61 +13,37 @@ class ListNode(object):
 		self.val = x
 		self.next = None
 
-class Solution(object):
-	def addTwoNumbers(self, l1, l2):	# bad thing
-		retLN = ListNode(0)
-		head = retLN
-		carry = 0
-
-		while(l1 or l2):
-			tempVal = l1.val + l2.val + retLN.val
-			tempVal, carry = tempVal%10, tempVal/10
-
-			if carry==1:
-				retLN.next = ListNode(1)
-			else:
-				retLN.next = ListNode(0)
-
-			retLN.val = tempVal
-
-			retLN = retLN.next
-			l1 = l1.next
-			l2 = l2.next
-
-		return head
-
-	def addTwoNumbers2(self, l1, l2):	# good thing
-
-		retLn = ListNode(0)
-		head = retLn
+class Solution:
+	def addTwoNumbers(self, l1: ListNode, l2: ListNode) -> ListNode:
+		retNode = ListNode(0)
+		headNode = retNode
 		tempVal = 0
 		carry = 0
 
 		while(l1 or l2):
-
 			if(l1):
 				tempVal += l1.val
 				l1 = l1.next
+
 			if(l2):
 				tempVal += l2.val
 				l2 = l2.next
 
 			carry, tempVal = divmod(tempVal, 10)
 
-			retLn.next = ListNode(tempVal)
-			retLn = retLn.next
+			retNode.next = ListNode(tempVal)
+			retNode = retNode.next
 			tempVal = carry
 
-		if carry == 1:
-			retLn.next = ListNode(1)
+		if(carry==1):
+			retNode.next = ListNode(1)
 
-		return head.next
+		return headNode.next
 
+if __name__ =='__main__':
+	lN1, lN1.next, lN1.next.next = ListNode(2), ListNode(4), ListNode(3)
+	lN2, lN2.next, lN2.next.next = ListNode(5), ListNode(6), ListNode(4)
 
-if __name__ == '__main__':
-	lN1, lN1.next, lN1.next.next = ListNode(2), ListNode(4), ListNode(5)
-	lN2, lN2.next, lN2.next.next = ListNode(5), ListNode(4), ListNode(6)
-	#resultVar = Solution().addTwoNumbers(lN1, lN2)
-	resultVar = Solution().addTwoNumbers2(lN1, lN2)
+	resultVar = Solution().addTwoNumbers(lN1, lN2)
 
-	print "{0} -> {1} -> {2}".format(resultVar.val, resultVar.next.val, resultVar.next.next.val)
+	print("{0} -> {1} -> {2}".format(resultVar.val, resultVar.next.val, resultVar.next.next.val))
