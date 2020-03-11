1460c4a73c9e78c4acfba8f64d1e1efe61d7f520
MyeongJun Go <myeongjun.ko@gmail.com>
2020-03-11 21:13:10 +0900

    reviewing algoriths leetcode(#3)

--- algorithms/LongestStringWithoutRepeating/solution.py
+++ algorithms/LongestStringWithoutRepeating/solution.py
@@ -11,24 +11,28 @@ Given "pwwkew", the answer is "wke", with the length of 3. Note that the answer 
 '''
 
 class Solution(object):
-	def lengthOfLongestSubstring(self, s):
-		tempString = ""
-		retStringList = []
-		sameFlag = 0
+	def lengthOfLongestSubstring(self, s: str) -> int:
+		idx = 0
+		loop2_idx = 0
+		temp_str = ''
+		long_str = []
 
-		for i in range(0, len(s)):
-			for j in range(0, len(tempString)):
-				if(s[i] == tempString[j]):
-					retStringList.append(tempString)
-					tempString = tempString[j+1:]
+		for loop_str in s:
+			temp_str = loop_str
+			idx += 1
+
+			for loop2_str in s[idx:]:
+				if s[idx+loop2_idx] in temp_str:
 					break
+				else:
+					temp_str += loop2_str
+					loop2_idx += 1
 
-			tempString += s[i]
+			long_str.append(temp_str)
+			loop2_idx = 0
 
-		retStringList.append(tempString)
-
-		return len(max(retStringList, key=len))
+		return len(max(long_str, key=len, default=''))
 
 if __name__=='__main__':
-	lenOfString = Solution().lengthOfLongestSubstring('abcabcbb')
+	lenOfString = Solution().lengthOfLongestSubstring('')
 	print(lenOfString)
