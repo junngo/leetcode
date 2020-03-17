da867527dd4d47951941856ca2e734a8ab21bdcf
MyeongJun Go <myeongjun.ko@gmail.com>
2020-03-17 11:40:58 +0900

    solving algorithms leetcode(#7- reverse digits of an integer in 32 bit)

--- algorithms/ReverseInteger/solution.py
+++ algorithms/ReverseInteger/solution.py
@@ -20,31 +20,29 @@ Note:
 Assume we are dealing with an environment which could only store integers within the 32-bit signed integer range
 For the purpose of this problem, assume that your function returns 0 when the reversed integer overflows.
 '''
-class Solution(object):
-    def reverse(self, x):
+class Solution:
+	def reverse(self, x: int) -> int:
+		minus_flag = False
+		pos_limit = 0x7fffffff
+		neg_limit =-0x80000000
 
-		toStr = str(x)
-		tempList = []
-		minusFlag = False
+		if x < 0:
+			x *= -1
+			minus_flag = True
 
-		if toStr[0] is '-':
-			minusFlag = True
+		num = int(str(x)[::-1])
 
-		for i in toStr:
-			tempList.append(i)
+		if minus_flag:
+			num *= -1
+			if (num & neg_limit) == neg_limit:
+				return num
 
-		tempList.reverse()
-		if minusFlag is True:
-			tempList.insert(0, '-')
-			tempList.pop()
-
-		retNum = int("".join(tempList))
-
-		if(abs(retNum) > ((1 << 31) - 1)):
-			return 0
 		else:
-			return retNum
+			if (num & pos_limit) == num:
+				return num
+
+		return 0
 
 if __name__=='__main__':
-	retNum = Solution().reverse(-120)
+	retNum = Solution().reverse(-123)
 	print(retNum)
