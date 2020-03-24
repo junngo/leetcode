10d3b5079ca2fde3f3fc25da495887467907552a
MyeongJun Go <myeongjun.ko@gmail.com>
2020-03-24 23:37:40 +0900

    solving algorithms leetcode(#10 - RegularExpressionMatching)

---
+++ algorithms/RegularExpressionMatching/solution.py
@@ -0,0 +1,66 @@
+'''
+Given an input string (s) and a pattern (p), implement regular expression matching with support for '.' and '*'.
+
+'.' Matches any single character.
+'*' Matches zero or more of the preceding element.
+The matching should cover the entire input string (not partial).
+
+Note:
+s could be empty and contains only lowercase letters a-z.
+p could be empty and contains only lowercase letters a-z, and characters like . or *.
+
+Example 1:
+Input:
+s = "aa"
+p = "a"
+Output: false
+Explanation: "a" does not match the entire string "aa".
+
+Example 2:
+Input:
+s = "aa"
+p = "a*"
+Output: true
+Explanation: '*' means zero or more of the preceding element, 'a'. Therefore, by repeating 'a' once, it becomes "aa".
+
+Example 3:
+Input:
+s = "ab"
+p = ".*"
+Output: true
+Explanation: ".*" means "zero or more (*) of any character (.)".
+
+Example 4:
+Input:
+s = "aab"
+p = "c*a*b"
+Output: true
+Explanation: c can be repeated 0 times, a can be repeated 1 time. Therefore, it matches "aab".
+
+Example 5:
+Input:
+s = "mississippi"
+p = "mis*is*p*."
+Output: false
+'''
+
+class Solution:
+    # Recursive
+    def isMatch1(self, s: str, p: str) -> bool:
+
+        first_match = False
+        if len(s) == 0 and len(p) == 0:
+            return True
+
+        if len(s) > 0 and len(p) > 0:
+            first_match = p[0] in [s[0], "."]
+
+        if len(p) >= 2 and p[1] == '*':
+            return (first_match and self.isMatch1(s[1:], p)) or self.isMatch1(s, p[2:])
+
+        else:
+            return first_match and self.isMatch1(s[1:], p[1:])
+
+if __name__ == '__main__':
+    my_regular = Solution().isMatch1('bbbb', 'abcde')
+    print(my_regular)
