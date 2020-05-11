987fb5c12b6cee04710503d6c152f3c0117a0db3
MyeongJun Go <myeongjun.ko@gmail.com>
2020-05-11 23:49:58 +0900

    solving algorithms leetcode(#20 - Valid Parentheses)

---
+++ algorithms/ValidParentheses/solution.py
@@ -0,0 +1,40 @@
+'''
+Given a string containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
+
+An input string is valid if:
+
+Open brackets must be closed by the same type of brackets.
+Open brackets must be closed in the correct order.
+Note that an empty string is also considered valid.
+
+Example 1:
+
+Input: "()"
+Output: true
+Example 2:
+
+Input: "()[]{}"
+Output: true
+'''
+
+class Solusion:
+    def isValid(self, s: str) -> bool:
+        stack = []
+        mapping = {")": "(", "}": "{", "]": "["}
+
+        for char in s:
+            if char in mapping:
+                top_element = stack.pop() if stack else '#'
+
+                if mapping[char] != top_element:
+                    return False
+
+            else:
+                stack.append(char)
+
+        return not stack
+
+
+if __name__=='__main__':
+	str = Solusion().isValid("()[]{}")
+	print(str)
