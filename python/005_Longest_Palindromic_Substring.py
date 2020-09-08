aac9b53b8077e840cfc3d5a62debd8ca10b18f65
MyeongJun Go <myeongjun.ko@gmail.com>
2020-09-09 01:40:09 +0900

    cleaning and moving #5 Longest Palindromic Substring

--- python/005_Longest_Palindromic_Substring.py
+++ python/005_Longest_Palindromic_Substring.py
@@ -13,24 +13,21 @@ Output: "bb"
 
 class Solution:
     def longestPalindrome(self, s: str) -> str:
-        idx = 0
-        temp_str = ''
-        ret_palindorme = []
+        temp_str, palindorme = "", []
 
+        for idx, ch in enumerate(s):
+            temp_str = ch
+            palindorme.append(temp_str)
 
-        for i in s:
-            idx += 1
-            temp_str = i
-            ret_palindorme.append(temp_str)
-
-            for j in s[idx:]:
+            for j in s[idx+1:]:
                 temp_str += j
 
                 #  checking palindrome
                 if temp_str[:] == temp_str[::-1]:
-                    ret_palindorme.append(temp_str)
+                    palindorme.append(temp_str)
 
-        return max(ret_palindorme, key=len, default='')
+        return max(palindorme, key=len, default='')
+
 
 if __name__=='__main__':
 	resultStr = Solution().longestPalindrome("a")
