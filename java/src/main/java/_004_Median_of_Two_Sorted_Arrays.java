2b292c3361f23f7b62c2dec89dc313fa54919a55
junngo <myeongjun.ko@gmail.com>
2020-10-03 16:51:52 +0900

    solving algorithms leetcode(#4 Median of Two Sorted Arrays - java)

--- java/src/main/java/_004_Median_of_Two_Sorted_Arrays.java
+++ java/src/main/java/_004_Median_of_Two_Sorted_Arrays.java
@@ -1,15 +1,29 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class _004_Median_of_Two_Sorted_Arrays {
     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
-        for (int i = 0; i < nums1.length; i++) {
-            System.out.println(nums1[i]);
+        List<Integer> list = new ArrayList<>();
+        for(int i : nums1) {
+            list.add(i);
+        }
+        for(int i : nums2) {
+            list.add(i);
         }
 
-        return 0.0;
+        list.sort(null);
+        int mid = list.size() / 2;
+        if ( list.size() % 2 == 0) {
+            return (list.get(mid-1) + list.get(mid)) / 2.0;
+        }
+
+        return list.get(mid);
     }
 
     public static void main(String[] args) {
         double ret = new _004_Median_of_Two_Sorted_Arrays().findMedianSortedArrays(
-                new int[] {1, 2, 3}, new int[] {2}
+                new int[] {1, 2}, new int[] {3, 4}
         );
+        System.out.println(ret);
     }
 }
