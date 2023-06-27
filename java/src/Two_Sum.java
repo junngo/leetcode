a306cfd1a91ec6ac03bddc4aea444e9949a6fc32
junngo <myeongjun.ko@gmail.com>
2023-06-27 16:47:57 +0900

    init project

---
+++ java/src/Two_Sum.java
@@ -0,0 +1,11 @@
+class Two_Sum {
+    public int[] twoSum(int[] nums, int target) {
+        System.out.println("hello world");
+        return null;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = [2,7,11,15];
+        int target = 9;
+    }
+}
