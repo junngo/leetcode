eec87ff69428e98296f244c2fcc5f546e1d501b8
junngo <myeongjun.ko@gmail.com>
2023-06-27 17:15:39 +0900

    added the Two Sum problem with the java

--- README.md
+++ README.md
@@ -6,4 +6,4 @@ LeetCode Algorithm
 ## Algorithms
 |  #  | Title                  |  Solution       | Difficulty    | Tag          | Note|
 |-----|----------------------- | --------------- | ------------- |--------------|-----|
-001 | [Two Sum](https://leetcode.com/problems/two-sum/) | [Python](./python/Two_Sum.py), [Java](./java/Two_Sum.java)| Easy         |Array||
+0001 | [Two Sum](https://leetcode.com/problems/two-sum/) | [Python](./python/Two_Sum.py), [Java](./java/src/Two_Sum.java)| Easy         |Array, Hash Table||
--- java/src/Two_Sum.java
+++ java/src/Two_Sum.java
@@ -1,11 +1,24 @@
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
 class Two_Sum {
     public int[] twoSum(int[] nums, int target) {
-        System.out.println("hello world");
-        return null;
+        Map<Integer, Integer> numMap = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            int value = target - nums[i];
+            if (numMap.containsKey(value)) {
+                return new int[] {numMap.get(value), i};
+            }
+            numMap.put(nums[i], i);
+        }
+        return new int[]{};
     }
 
     public static void main(String[] args) {
-        int[] nums = [2,7,11,15];
+        int[] nums = {2, 7, 11, 15};
         int target = 9;
+        int[] result = new Two_Sum().twoSum(nums, target);
+        System.out.println(Arrays.toString(result));
     }
 }
